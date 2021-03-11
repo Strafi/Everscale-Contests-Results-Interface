e534578f4475b22d103fc2bf95ffe28a37593ce7
Anton Solodkov <anton.solodkov@sharekey.com>
2021-03-11 03:01:43 +0300

    add reward calculation for submissions with the same score

--- src/components/Contest/ContestTableSubmissionsBody.js
+++ src/components/Contest/ContestTableSubmissionsBody.js
@@ -4,20 +4,26 @@ const ContestTableSubmissionsBody = memo(({ contestSubmissions }) => {
 	let totalReward = 0;
 
 	const submissionsToRender = contestSubmissions.map(submission => {
-		totalReward += submission.reward;
+		if (typeof submission.reward === 'number' && !Number.isNaN(submission.reward))
+			totalReward += submission.reward;
+
+		const scoreToShow = submission.score.toFixed(2);
 
 		return (
 			<tr key={submission.participantAddress}>
 				<td>{submission.place}</td>
 				<td>{submission.reward}</td>
 				<td>{submission.id}</td>
-				<td>{submission.score}</td>
+				<td>{scoreToShow}</td>
 				<td>{submission.acceptAmount}</td>
 				<td>{submission.rejectAmount}</td>
 				<td>{submission.participantAddress}</td>
 			</tr>
 		)
 	});
+
+	if (!submissionsToRender.length)
+		return (<div>no subm</div>)
 
 	return (
 		<tbody>
--- src/helpers/processSubmissionsInfo.js
+++ src/helpers/processSubmissionsInfo.js
@@ -10,12 +10,15 @@ function processSubmissionsInfo(initialSubmissionsInfo, contestRewards) {
 		if (isRejected) {
 			newSubmInfo.reward = 0;
 			newSubmInfo.score = 0;
+			newSubmInfo.isRejected = true;
 		} else {
 			const marksSum = newSubmInfo.marks.reduce((previousValue, currentValue) => {
 				return previousValue + parseInt(currentValue, 10);
 			}, 0);
 
-			newSubmInfo.score = marksSum / newSubmInfo.marks.length;
+			const score = marksSum / newSubmInfo.marks.length;
+			const normalizedScore = score.toFixed(2);
+			newSubmInfo.score = +normalizedScore;
 		}
 
 		newSubmInfo.acceptAmount = newSubmInfo.marks.length;
@@ -24,11 +27,27 @@ function processSubmissionsInfo(initialSubmissionsInfo, contestRewards) {
 	});
 
 	const sortedSubmissions = sortSubmissions(submissions, SORT_BY_VALUES.SCORE);
+	const submissionsWithFullInfo = getSubmissionsWithFullInfo(sortedSubmissions, contestRewards);
+
+	return submissionsWithFullInfo;
+}
+
+function getSubmissionsWithFullInfo(sortedSubmissions, contestRewards) {
 	const parsedRewards = parseRewards(contestRewards);
+	const submissionsWithSameScore = {};
+	let prevReward;
+
 	const submissionsWithFullInfo = sortedSubmissions.map((subm, index, initialArray) => {
-		// const prevSubm = initialArray[index - 1];
+		const prevSubm = initialArray[index - 1];
 		const place = index + 1;
-		const reward = parsedRewards[place];
+		const reward = parsedRewards[place] || 0;
+
+		if (prevSubm && prevSubm.score === subm.score && subm.score !== 0) {
+			submissionsWithSameScore[index - 1] = prevReward;
+			submissionsWithSameScore[index] = reward;
+		}
+
+		prevReward = reward;
 
 		return {
 			...subm,
@@ -36,6 +55,43 @@ function processSubmissionsInfo(initialSubmissionsInfo, contestRewards) {
 			reward,
 		}
 	})
+
+	let tempToCollectSequence = {}, prevSubmIndex;
+	const lastSequenceIndex = +Object.keys(submissionsWithSameScore).pop();
+	for (const submIndex in submissionsWithSameScore) {
+		const numberSubmIndex = +submIndex;
+		const isLastIndex = numberSubmIndex === lastSequenceIndex;
+		const shouldCollectSequence = (!prevSubmIndex
+			|| (
+				prevSubmIndex + 1 === numberSubmIndex
+				&& submissionsWithFullInfo[prevSubmIndex].score === submissionsWithFullInfo[numberSubmIndex].score
+			))
+			&& !isLastIndex
+
+		if (shouldCollectSequence) {
+			tempToCollectSequence[numberSubmIndex] = submissionsWithSameScore[numberSubmIndex];
+		} else {
+			if (isLastIndex)
+				tempToCollectSequence[numberSubmIndex] = submissionsWithSameScore[numberSubmIndex];
+
+			const rewardsForSequence = Object.values(tempToCollectSequence);
+			const totalRewardForSequence = rewardsForSequence.reduce((previousValue, currentValue) => {
+				return previousValue + currentValue;
+			}, 0);
+
+			const rewardForSubmInSequence = totalRewardForSequence / rewardsForSequence.length;
+
+			for (const submSeqIndex in tempToCollectSequence) {
+				submissionsWithFullInfo[submSeqIndex].reward = rewardForSubmInSequence;
+			}
+
+			tempToCollectSequence = {
+				[numberSubmIndex]: submissionsWithSameScore[numberSubmIndex]
+			};
+		}
+
+		prevSubmIndex = numberSubmIndex;
+	}
 
 	return submissionsWithFullInfo;
 }
